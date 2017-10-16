1600d11c099cb605159353640d001ab4f4a19b5a
Sharon <viren.tech.developer@gmail.com>
2017-10-16 09:38:01 +0800

    20171016-Sharon update again.

--- app/src/main/java/com/example/student/order/Sec.java
+++ app/src/main/java/com/example/student/order/Sec.java
@@ -2,9 +2,11 @@ package com.example.student.order;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class Sec extends AppCompatActivity {
+public class Sec extends AppCompatActivity implements View.OnClickListener{
     private Button btnWaiter, btnChef, btnCashier;
 
     @Override
@@ -16,4 +18,22 @@ public class Sec extends AppCompatActivity {
         btnCashier = (Button) findViewById(R.id.cashier);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.waiter:
+                ToastMsg("You are Waiter!");
+                break;
+            case R.id.chef:
+                ToastMsg("You are Chef!");
+                break;
+            case R.id.cashier:
+                ToastMsg("You are Cashier!");
+                break;
+        }
+    }
+
+    public void ToastMsg(String strMsg){
+        Toast.makeText(this, strMsg, Toast.LENGTH_LONG).show();
+    }
 }
