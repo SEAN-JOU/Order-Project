3ad8cb62d3742971f2044bf1fe028657d0b1ca0d
Sharon <viren.tech.developer@gmail.com>
2017-10-16 09:31:38 +0800

    20171016-Sharon update.

--- app/src/main/java/com/example/student/order/Sec.java
+++ app/src/main/java/com/example/student/order/Sec.java
@@ -2,13 +2,18 @@ package com.example.student.order;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Sec extends AppCompatActivity {
+    private Button btnWaiter, btnChef, btnCashier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sec);
+        btnWaiter = (Button) findViewById(R.id.waiter);
+        btnChef = (Button) findViewById(R.id.chef);
+        btnCashier = (Button) findViewById(R.id.cashier);
     }
 
 }
