cc679006bd0ed439a6a40afb0e5ccf843bc9a2ab
Sharon <viren.tech.developer@gmail.com>
2017-10-25 17:38:05 +0800

    20171025-Sharon update for cashier

--- app/src/main/java/com/example/student/order/Cashier.java
+++ app/src/main/java/com/example/student/order/Cashier.java
@@ -3,27 +3,33 @@ package com.example.student.order;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import static android.graphics.Color.parseColor;
 
 
 public class Cashier extends AppCompatActivity {
     private TextView txtNumber, txtName, txtDate;
     TabHost mTabHost;
+    private TabHost.TabSpec spec;
     ListView inlistview,outlistview;
-    InCashierAdapter incashierAdapter,adapter;
+    InCashierAdapter incashierAdapter;
     OutCashierAdapter outcashierAdapter;
     ArrayList<Order> out_list,in_list;
     Order out_orderlist,in_orderlist;
     int in_index,out_index;
+    int RECODE_IN=1001;
+    int RECODE_OUT=1002;
 
 
 
@@ -34,18 +40,9 @@ public class Cashier extends AppCompatActivity {
 
         findView();
         readSharePreferences();
-
-        mTabHost = (TabHost)findViewById(R.id.tabhost);
-        mTabHost.setup();
-
-        TabHost.TabSpec tab1 = mTabHost.newTabSpec("0");
-        tab1.setIndicator("外帶");
-        tab1.setContent(R.id.tab1);
-        mTabHost.addTab(tab1);
-
-        mTabHost.addTab(mTabHost.newTabSpec("1")
-                .setIndicator("內用")
-                .setContent(R.id.tab2));
+        tabSettings("eatIn",R.id.tabIn,"內用");
+        tabSettings("takeAway",R.id.tabOut,"外帶");
+        changeTabBackgroundOnChanged();
     }
 
     public void findView(){
@@ -59,9 +56,11 @@ public class Cashier extends AppCompatActivity {
         txtDate.setText(strDate);
         txtNumber = (TextView) findViewById(R.id.txtNumber);
         txtName = (TextView) findViewById(R.id.txtName);
+        mTabHost = (TabHost)findViewById(R.id.tabhost);
+        mTabHost.setup();
 
         out_list=new ArrayList<>();
-        //out_orderlist=new Order(10,"王",1,1);
+        out_orderlist=new Order("1061025001","王小名",100,3);
         out_list.add(out_orderlist);
         outcashierAdapter=new OutCashierAdapter(this,out_list);
         outlistview.setAdapter(outcashierAdapter);
@@ -69,18 +68,14 @@ public class Cashier extends AppCompatActivity {
         //外帶類型
 
         in_list=new ArrayList<>();
-        /*in_orderlist=new Order("Ａ",1,1,1);
+        in_orderlist=new Order("A","1061025002",100,2);
         in_list.add(in_orderlist);
-        in_orderlist=new Order("Ａ",1,1,1);
+        in_orderlist=new Order("B","1061025003",200,2);
         in_list.add(in_orderlist);
-        in_orderlist=new Order("Ａ",1,1,1);
+        in_orderlist=new Order("C","1061025004",300,2);
         in_list.add(in_orderlist);
-        in_orderlist=new Order("Ａ",1,1,1);
+        in_orderlist=new Order("D","1061025005",400,2);
         in_list.add(in_orderlist);
-        in_orderlist=new Order("Ａ",1,1,1);
-        in_list.add(in_orderlist);
-        in_orderlist=new Order("Ａ",1,1,1);
-        in_list.add(in_orderlist);*/
 
         incashierAdapter=new InCashierAdapter(this,in_list);
         inlistview.setAdapter(incashierAdapter);
@@ -90,29 +85,27 @@ public class Cashier extends AppCompatActivity {
 
 
 
-        inlistview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        inlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int a, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int a, long l) {
 
                 in_index=a;
                 Intent it =new Intent();
                 it.setClass(Cashier.this,Menu_context.class);
-                startActivityForResult(it,101);
+                startActivityForResult(it,RECODE_IN);
 
-            return true;
             }});
 
-        outlistview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        outlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
 
                 out_index=i;
-                Intent it =new Intent();
-                it.setClass(Cashier.this,Menu_context.class);
-                startActivityForResult(it,100);
+                Intent it2 =new Intent();
+                it2.setClass(Cashier.this,Menu_context.class);
+                startActivityForResult(it2,RECODE_OUT);
 
-                return true;
             }});
 
 
@@ -127,22 +120,64 @@ public class Cashier extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK && requestCode == 100){
+        if(resultCode == RESULT_OK && requestCode == RECODE_OUT){
 
-           int status_out =Integer.valueOf(data.getStringExtra("已結帳"));
+            int status_out =Integer.valueOf(data.getStringExtra("已結帳"));
             ((Order)out_list.get(out_index)).i_status=status_out;
             outcashierAdapter.notifyDataSetChanged();
         }
 
-         else if(resultCode == RESULT_OK && requestCode == 101) {
+        else if(resultCode == RESULT_OK && requestCode == RECODE_IN) {
 
             int status_in =Integer.valueOf(data.getStringExtra("已結帳"));
             ((Order)in_list.get(in_index)).i_status=status_in;
             incashierAdapter.notifyDataSetChanged();
 
 
-        }}}
+        }
+    }
 
+    //設定tab
+    public void tabSettings(String tabTagName, int id,String name){
+        spec=mTabHost.newTabSpec(tabTagName);
+        spec.setContent(id);
+        spec.setIndicator(name);
+        mTabHost.addTab(spec);
+        setTabBackground();
+    }
+
+    //設定tab背景色和高度
+    public void setTabBackground(){
+        //初次設定
+        for(int i=0; i<mTabHost.getTabWidget().getTabCount();i++){
+            TextView tv=mTabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tv.setTextColor(parseColor("#ffffff"));
+            tv.setTextSize(25);
+            mTabHost.getTabWidget().getChildAt(mTabHost.getCurrentTab()).setBackgroundColor(parseColor("#99fdad0c"));
+            mTabHost.getTabWidget().getChildAt(i).getLayoutParams().height = 140;
+            if(i==0){
+                mTabHost.getTabWidget().getChildAt(i).setBackgroundColor(parseColor("#99ff504d"));
+            }else if(i==1){
+                mTabHost.getTabWidget().getChildAt(i).setBackgroundColor(parseColor("#99019e94"));
+            }
+        }
+        TextView tv = (TextView) mTabHost.getCurrentTabView().findViewById(android.R.id.title);
+        tv.setTextColor(parseColor("#000000"));
+        mTabHost.getTabWidget().getChildAt(mTabHost.getCurrentTab()).setBackgroundColor(parseColor("#99fdad0c"));
+    }
+
+    //切換tab時設定background顏色
+    public void changeTabBackgroundOnChanged(){
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+                setTabBackground();
+
+            }
+        });
+    }
+
+}
 
 
 
