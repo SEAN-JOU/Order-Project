113f0fc322f37583085788ae4254db067ef5b754
maggieww1111 <maggieww1111@gmail.com>
2017-12-06 16:28:55 +0800

    單號重複問題修正

--- app/src/main/java/com/example/student/order/Chef.java
+++ app/src/main/java/com/example/student/order/Chef.java
@@ -14,6 +14,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Chef extends AppCompatActivity {
     private static final String TAG = "Chef";
@@ -21,7 +23,7 @@ public class Chef extends AppCompatActivity {
     private ChefExpandListAdapter adapter;
     private ArrayList<Order> orderList;
     ArrayList<Order> arrShowOrder,arrTmpSaveOrder;
-
+    TreeSet<Integer> ts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,29 +39,33 @@ public class Chef extends AppCompatActivity {
         orderList = new ArrayList<>();
         arrShowOrder=new ArrayList<>();
         arrTmpSaveOrder=new ArrayList<>();
+        ts=new TreeSet<>();
     }
 
     public void setCookList() {
         adapter = new ChefExpandListAdapter(this, arrShowOrder);
         cookList.setAdapter(adapter);
-
         cookList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView expandableListView, View view, int groupPosition, int childPosition, long l) {
-                OrderItems oi = (OrderItems)adapter.getChild(groupPosition,childPosition);
+                OrderItems oi= (OrderItems)adapter.getChild(groupPosition,childPosition);
                 oi.isCooked=true;
-                Order changeOrder=orderList.get(groupPosition); //取得修改狀態的order物件
-                changeOrder.i_status=2;
+                //先把有改出菜狀態的orderitems存入一個暫存的arraylist中
+                ArrayList<OrderItems> tmp=new ArrayList<OrderItems>();
+                tmp.add(oi);
+                Order o = (Order) adapter.getGroup(groupPosition);
+                o.i_status=2;
+                //把剛剛暫存的arraylist放到這邊修改過出菜狀態的order中
+                o.getOrderItems().add(tmp);
                 ArrayList<Order> tmpArray=new ArrayList<Order>(); //建立一個暫存陣列
-                tmpArray.add(changeOrder);   //把修改好的Order物件放入陣列
+                tmpArray.add(o);   //把修改好的Order物件放入陣列
                 Gson gson = new Gson();
                 String OrderStr = gson.toJson(tmpArray);  //把陣列轉成json字串
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference order_Ref = database.getReference("order").child(changeOrder.getI_Order()); //取得要修改的訂單單號
+                DatabaseReference order_Ref = database.getReference("order").child(o.getI_Order()); //取得要修改的訂單單號
                 order_Ref.setValue(OrderStr);
                 Log.w("OrderStr->", OrderStr);
-                Log.d("status","status2:"+oi.isCooked);
-                Log.d("status","status3:"+ changeOrder.i_status);
+                Log.d("status","status3:"+ o.i_status);
                 adapter.notifyDataSetChanged();
                 return true;
             }
@@ -79,13 +85,11 @@ public class Chef extends AppCompatActivity {
                 arrTmpSaveOrder.clear();
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+
                 for (DataSnapshot data : dataSnapshot.getChildren()) {  //取得order下的子項目
                     String dbOrder = data.getKey(); //訂單編號
-                    Log.w("FireBaseTraining", "No = " + dbOrder);
-                    //strKeyJason.add(dbOdrder)
                     String order_string = data.getValue().toString();   //一筆訂單的 Json Data
                     strOrderJason.add(order_string);
-                    Log.w("FireBaseTraining", "value = " + order_string);
                 }
 
                 for (String strOrder: strOrderJason) {
@@ -94,39 +98,30 @@ public class Chef extends AppCompatActivity {
                     orderArray = gson.fromJson(strOrder, Order[].class); //把strOrderJason的全部資料都丟到Order陣列中
                     orderList.add(orderArray[0]);
                 }
-                Log.d("fire","size"+orderList.size());
                 for(int i=0;i<orderList.size();i++){
-                    //Log.d("fire",""+orderList.get(i).str_Order);
-                    //Log.d("fire",""+orderList.get(i).str_Flag);
-                    //Log.d("fire",""+orderList.get(i).i_status);
+
                     if(orderList.get(i).i_status!=3){
                         Log.d("fire",""+orderList.get(i).str_Order);
                         arrTmpSaveOrder.add(orderList.get(i));
                     }
                 }
 
-                for(int i=0;i<arrTmpSaveOrder.size();i++){   //未結帳的所有單單號log
-                    Log.d("fire","arrTmpSaveOrder"+arrTmpSaveOrder.get(i).str_Order);
-                }
                 for(int i=0;i< arrTmpSaveOrder.size();i++){  //orderItems.size()座位數
-                    Log.d("fire2","arrTmpSaveOrder i="+i + arrTmpSaveOrder.get(i).orderItems.size()); //每筆單的座位數=>內用=4，外帶=0
                     for(int j=0;j<arrTmpSaveOrder.get(i).orderItems.size();j++){
-                        Log.d("fire3","arrTmpSaveOrder j=" + j ); //j只有第0筆
-                        Log.d("fire4","arrTmpSaveOrder j=" + j + "---" +arrTmpSaveOrder.get(i).orderItems.get(j).size()); //因為j只抓的到第0筆，所以這邊的orderItems.get(j).size()會是第0筆的size
                         for(int k=0;k<arrTmpSaveOrder.get(i).orderItems.get(j).size();k++) {
-                           /* if(arrTmpSaveOrder.get(i).orderItems.get(j).size()==0){
-                                continue;
-                            }else */if (arrTmpSaveOrder.get(i).orderItems.get(j).get(k).isCooked != true) {
-                                arrShowOrder.add(arrTmpSaveOrder.get(i));
-                                break;
+                            int index=0;
+                            if (arrTmpSaveOrder.get(i).orderItems.get(j).get(k).isCooked != true) {
+                                index=i; //判斷如果所點菜品有一項未出菜，就抓該筆序號丟給index
+                                ts.add(index);
                             }
-                            break;
                         }
-                        //break; 如果加break只要第一個座位沒點餐，就會break跳出不會繼續，就會少顯示一筆定單
                     }
                 }
+                Log.d("fire5","hs" +ts.toString());
+                for (int x:ts) {  //要放在for迴圈外面這樣才不會重複加了好幾次，用hashset把重複加入的單給排除掉
+                    arrShowOrder.add(arrTmpSaveOrder.get(x)); //再用暫存的order Arraylist來取得該項序號的order物件存入arrShowOrder
+                }
 
-                //Log.w("FireBaseTraining", "arrShowOrder = " + arrShowOrder);
                 adapter.notifyDataSetChanged();
             }
 
